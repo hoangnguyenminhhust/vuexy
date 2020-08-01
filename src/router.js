c7f55c8829217e4618333507d94fbe0e53c49f54
Nguyễn Minh Hoàng <hoangnguyenminh.hust@gmail.com>
2020-08-01 20:29:43 +0700

    update

--- src/router.js
+++ src/router.js
@@ -34,7 +34,7 @@ const router = new Router({
     {
       path: '',
       component: () => import('./layouts/main/Main.vue'),
-      beforeEnter: checkAuthetication,
+      beforeEnter: checkAuthetication(),
       children: [
 
         {
